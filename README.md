0c13b6adfcbfddaf8f15ab4547b324de697025cf
Victor Martin <victor.martin.alvarez@oracle.com>
2019-09-03 16:47:39 +0100

    Add Dredd

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hello Apiary
 Basic API example for Apiary
+
+## Install dependencies
+
+`npm install`
+
+## Run Dredd
+
+`npm test`
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
 
 app.get('/health', (req, res) => res.json({ status: 'OK' }))
 
